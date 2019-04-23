85e941280554c8ff6b1a5569f1b4058874ca2885
Iqbalalfikri01 <49835976+Iqbalalfikri01@users.noreply.github.com>
2019-04-23 17:03:50 +0700

    Add files via upload

---
+++ Run.sh
@@ -0,0 +1,330 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+echo
+echo
+python2 meizu.py
+echo
+clear
+echo
+echo $red"          Tool akan di install otomatis"
+echo $red"            harap sabar menunggu yaa Beb :v"   
+sleep 1
+echo '''\a
+\033[34;1m                              __
+\033[34;1m                             /   |
+\033[34;1m                             |    |
+\033[34;1m                             |    |
+\033[34;1m                            _| _ |_ '''
+sleep 1
+apt update && apt upgrade
+apt install nano
+apt install git
+pkg install python python2 vim figlet curl
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install otomatis"
+echo $red"            harap sabar menunggu yaa Beb :v"
+sleep 1
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            |___ \
+\033[32;1m                             __) |
+\033[32;1m                            / __/
+\033[32;1m                           |_____| '''
+sleep 1
+apt install php
+pip2 install mechanize
+pip2 install lolcat
+pip2 install requests
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install otomatis"
+echo $red"            harap sabar menunggu yaa Beb :v"
+sleep 1
+echo '''\a
+\033[35;1m                            _____
+\033[35;1m                           |___ /
+\033[35;1m                             |_ \
+\033[35;1m                            ___) |
+\033[35;1m                           |____/ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install otomatis"
+echo $red"            harap sabar menunggu yaa Beb:* :v"
+sleep 1
+echo '''\a
+\033[33;1m                            _  _
+\033[33;1m                           | || |
+\033[33;1m                           | || |_
+\033[33;1m                           |__   _|
+\033[33;1m                              |_| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install Otomatis"
+echo $red"            harap sabar menunggu yaa Beb:* :v"
+sleep 1
+echo '''\a
+\033[36;1m                             ____
+\033[36;1m                            | ___|
+\033[36;1m                            |___ \
+\033[36;1m                             ___) |
+\033[36;1m                            |____/ '''
+sleep 1
+clear
+echo
+figlet Mr.Mystry Tools
+echo $pur"______________________"$cy" ___________________________"
+echo $i"CWD"$pu"  AUTHOR  :  Mr.Mytry  "$i"                               CBS"
+echo $i"CWD"$pu"  WhatsApp:  08121940xxxx"$i"                    CBS"
+echo $i"CWD"$pu"  GMAIL   :  mufik010704@gmail.com"$i" CBS"
+echo $i"CWD"$pu"  YOUTUBE :  iqbalalfikri"$i"                 CBS"
+echo $pur"______________________"$cy"____________________________"
+echo
+figlet Menu Tools | lolcat
+echo $cy"MENU TOOLS:"
+echo $b "=========================" | lolcat
+echo $b "1.   REPORT FB";
+echo $b "=========================" | lolcat
+echo $b "2.   BRUTE FORCE";
+echo $b "=========================" | lolcat
+echo $b "3.   BRUTE FORCE FB CRACKER";
+echo $b "=========================" | lolcat
+echo $b "4.   MBF";
+echo $b "=========================" | lolcat
+echo $b "5.   CLONING YAHOO";
+echo $b "=========================" | lolcat
+echo $b "6.   DDOS HAMMER";
+echo $b "=========================" | lolcat
+echo $b "7.   PROFILEGUARD FB";
+echo $b "=========================" | lolcat
+echo $b "8.   BOT REACTION FACEBOOK";
+echo $b "=========================" | lolcat
+echo $b "9.   SPAM SMS UNLIMITED";
+echo $b "=========================" | lolcat
+echo $b "10.  SPAM WHATSHAPP";
+echo $b "=========================" | lolcat
+echo $b "11.   YOUTUBE-DL";
+echo $b "=========================" | lolcat
+echo $b "12.   LACAK LOKASI";
+echo $b "=========================" | lolcat
+echo $b "13.  ShellPhish";
+echo $b "=========================" | lolcat
+echo $b "14.  DarkFly (540 Tools)";
+echo $b "=========================" | lolcat
+echo $b "15.  Tools TUANB4DUT";
+echo $b "=========================" | lolcat
+echo $b "16.  Tools Mr.Ryuxhan";
+echo $b "=========================" | lolcat
+echo $b "17.  Bikin Virus";
+echo $b "=========================" | lolcat
+echo $b "18.  Kumpulan Brute Force Fb";
+echo $b "=========================" | lolcat
+echo $b "19.  Bikin Script Deface";
+echo $b "=========================" | lolcat
+echo $b "20.  20 Tools Mantap Jiwa";
+echo $b "=========================" | lolcat
+echo $b "21.  BomSms";
+echo $b "=========================" | lolcat
+echo $b "22.  Admin_LoginTermux";
+echo $b "=========================" | lolcat
+echo $b "23.  Tools B4J1n64n v6";
+echo $b "=========================" | lolcat
+echo $i"["$me"0"$i"]"$ku" EXIT";
+echo $b "=========================" | lolcat
+echo
+echo $pur"~root#["$pur"Masukkan pilihan anda"$pur"]"
+read -p"Pilih Nomor>> " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/VanGanteng/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Senitopeng/fbbrute.git
+cd fbbrute
+python2 jomblo.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3.git
+cd Fb-Cracker-v.3
+python2 crack.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/tikuskecil/multi-bruteforce-facebook.git
+cd multi-bruteforce-facebook
+python2 MBF.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/wahyuandhika/YahooCloning.git
+cd YahooCloning
+pip2 install requests mechanize
+pip2 install requirements
+python2 cloning.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/cyweb/hammer
+cd hammer
+python hammer.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/jaxBCD/FBshield.git
+cd FBshield
+python2 guard.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/edi-nasa12/ROBOT.git
+cd ROBOT
+python2 blackbot.py
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+chmod +x LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/4L13199/LITESPAM.git
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/rahmadxyz/sub-bot.git
+cd sub-bot
+php sub-bot.php
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/locator.git
+cd locator
+bash locator.sh
+fi
+
+if [$pil = 13 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/shellphish
+cd shellphish
+bash shellphish.sh
+fi
+
+if [$pil = 14 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Ranginang67/DarkFly-Tool
+cd DarkFly-Tool
+python2 install.py
+DarkFly
+fi
+
+if [$pil = 15 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+$git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+cd TOOLSINSTALLERv4
+
+if [ $pil = 0 ]
+then
+clear
+figlet -f slant "W A I T"|Ulolcat
+sleep 2
+echo $cy"Terima Kasih sudah mengunakan Tools ini"
+sleep 2
+echo $i"Silahkan di pakai dengan bijak, Semoga bermanfaat Buat anda"
+sleep 2
+echo $pur"Bila Ada Kesalahan Anda Bisa Nanya Melalui Via"
+sleep 2
+echo $ku"Facebook :"$i" Iqbal AlFikri"
+echo $ku"YouTube  :"$i" iqbalalfikri"
+echo $ku"WhatsApp :"$i" 08121940xxxx"
+sleep 2
+echo $pur"Terima kasih Yang Sudah Support dengan Tools ini"
+sleep 2
+echo $pur"SUBSCRIBE MY CHANNEL { iqbalalfikri }"
+exit
+fi
